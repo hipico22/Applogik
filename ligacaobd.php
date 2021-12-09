254b86a0c43d32e884b587aac155c2431bf3ef78
hipico22 <hipico2003@gmail.com>
2021-12-09 22:51:55 +0000

    Update ligacaobd.php

--- ligacaobd.php
+++ ligacaobd.php
@@ -1,13 +1,13 @@
 <?php
 // Liga ao MySQL 'localhost','root'
-$ligaBD = mysqli_connect('applogik_1','oratoroeuarolha');
+$ligaBD = mysqli_connect('localhost','root');
 
 if (!$ligaBD){
 echo "<br>Erro: Não foi possivel escolher estabelecer ligação com o MySQL";
 exit;
 }
 // Liga a BD
-$escolhaBD = mysqli_select_db($ligaBD,'applogik_1');
+$escolhaBD = mysqli_select_db($ligaBD,'applogik');
 if (!$escolhaBD){
 echo "<br>Erro: Ao escolher a BD";
 exit;
